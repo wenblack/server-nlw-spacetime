6ba3a4e0459ae5567d9f85a330355c8cdc77c2f3
wenblack <wenblack012@gmail.com>
2023-05-15 22:28:20 -0300

    ⚙️ Install and import Eslint Config ⚙️

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import fastify from 'fastify'
 const app = fastify()
 
 app.get('/hello', () => {
-  return "Hello World"
+  return 'Hello World'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('👻 Server running on http://localhost:3333 👻')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('👻 Server running on http://localhost:3333 👻')
+  })
